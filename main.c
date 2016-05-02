abde875599d97b73642bbb5c4a8433ab66077046
Alexandre Lafaye <lafa.alexandre@gmail.com>
2016-05-02 12:40:00 +0200

    final version

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "crypto.h"
 
-#define FILE_NAME_LIMIT 31
+#define LIMIT 31
 
 /* Arguments should be given following this format:
  * Encryption:
@@ -17,11 +17,10 @@
 int main(int argc, const char * * argv){
     /* Variable declaration */
     FILE *in_file, *out_file;
-    char in_file_name[FILE_NAME_LIMIT], out_file_name[FILE_NAME_LIMIT];
-
-    char * password, accept;
+    char * in_file_name,  * out_file_name, * password, accept;
     /* Mode: 0 for encryption, 1 for decryption */
-    int mode;
+    int mode, in_file_name_length, out_file_name_length, password_length;
+
 
     /* Get args */
     if(argc != 4 && argc != 6){
@@ -29,17 +28,36 @@ int main(int argc, const char * * argv){
 	return EXIT_FAILURE;
     }
     else{
-	strcpy(in_file_name  , argv[2]);
-	strcpy(out_file_name  , argv[3]);
+	/*To correctly set the string size */
+	in_file_name_length = strlen(argv[2]);
+	out_file_name_length = strlen(argv[3]);
+	in_file_name = malloc(in_file_name_length * sizeof(char));
+	if(in_file_name == NULL){
+	    printf("Le nom du fichier d'entrée n'est pas valide!");
+	    return EXIT_FAILURE;
+	}
+	out_file_name = malloc(out_file_name_length * sizeof(char));
+	if(out_file_name == NULL){
+	    printf("Le nom du fichier de sortie n'est pas valide!");
+	    return EXIT_FAILURE;
+	}
+
+	strcpy(in_file_name, argv[2]);
+	strcpy(out_file_name, argv[3]);
     }
 
     /* 
      * Check if password is inputed by the user
      * If not use default password "test1234"
-     * TODO specify password lenght
+     * TODO specify password length
      */
     if(argc == 6){
 	if(strcmp(argv[4], "-p")==0){
+	    password = malloc(password_length * sizeof(char));
+	    if(password == NULL){
+		printf("Le mot de passe n'est pas valide!");
+		return EXIT_FAILURE;
+	    }
 	    strcpy(password, argv[5]);
 	}
 	else{
@@ -48,15 +66,24 @@ int main(int argc, const char * * argv){
 	}
     }
     else{
-	password = "test1234";
+	/* A basic password is provided to rando if the user is not giving
+	 * it's own
+	 */
+	password = malloc(password_length * sizeof(char));
+	if(password == NULL){
+	    printf("Le mot de passe n'est pas valide!");
+	    return EXIT_FAILURE;
+	}
+	strcpy(password, "Justarandomandlongstring");
     }
 
+    /* Mode check an printing out to stdr the expected result */
     if(strcmp(argv[1], "-c")==0){
-	printf("encryption mode\n");
+	printf("Encryption mode\n");
 	mode = 0;
     }
     else if(strcmp(argv[1], "-d")==0){
-	printf("decryption mode\n");
+	printf("Decryption mode\n");
 	mode = 1;
     }
     else{
@@ -75,6 +102,7 @@ int main(int argc, const char * * argv){
     /* Then check if output file exists */
     if((out_file = fopen(out_file_name, "rb")) != NULL){
 	fclose(out_file);
+	/* Asking if the file need to be deleted */
 	printf("Le fichier de sortie existe déjà, l'écraser? [y/n] : ");
 
 	scanf("%c", &accept);
@@ -94,6 +122,7 @@ int main(int argc, const char * * argv){
 	printf("Echec de l'ouverture du fichier de sortie!");
 	return EXIT_FAILURE;
     }
+
 
     if(mode==0){
 	encrypt(in_file, out_file, password);
